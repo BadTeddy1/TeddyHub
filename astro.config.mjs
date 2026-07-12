f41d15cab35bc117ff5dc19186bcedbdca97907c
BadTeddy1 <phisherman400@gmail.com>
2026-07-12 09:11:55 +0100

    Added SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,18 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  import sitemap from "@astrojs/sitemap";
+  export default defineConfig({
+
+site:"https://teddyhub-eight.vercel.app",
+
+integrations:[
+
+react(),
+
+sitemap()
+
+]
+
 });
+
